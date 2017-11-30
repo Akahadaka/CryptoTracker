aeb83f45b6da8bb447e8400de10cdbe99ee08448
Andrew Nash <akahadaka@gmail.com>
2017-11-30 09:40:55 +0200

    Set up Store and Content container

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react';
 import {
   StyleSheet,
   Text,
   View
 } from 'react-native';
-import { Header } from './src/components';
+import { Provider } from 'react-redux';
+
+import Store from './src/Store';
+import {
+  Header,
+  CryptoContainer
+} from './src/components';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.headerContainer}>
-        <Header style={styles.header}/>
-      </View>
+      <Provider store={Store}>
+        <View style={styles.headerContainer}>
+          <Header style={styles.header}/>
+          <CryptoContainer />
+        </View>
+      </Provider>
     );
   }
 }
