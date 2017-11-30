72892f29bd976d58e7ab6ae6712dca8a2267bf4b
Andrew Nash <akahadaka@gmail.com>
2017-11-30 12:58:33 +0200

    Coin card and icons

---
+++ src/components/CoinCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+const CoinCard = (props) => {
+  return (
+    <View>
+      <Image />
+      <Text>{props.symbol}</Text>
+      <Text>{props.price_usd}</Text>
+      <Text>{props.percent_change_24h}</Text>
+    </View>
+  )
+}
+
+export default CoinCard;
